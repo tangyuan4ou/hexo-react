204bb10ad123a96873125d6fe7d9f63f2441c983
tangyuan4ou <tangyuan_youyou@163.com>
2017-11-03 15:09:01 +0800

    add blogPage title,data

--- src/route/archives/blog/blog.jsx
+++ src/route/archives/blog/blog.jsx
@@ -12,7 +12,21 @@ class Blog extends Component{
 
   render () {
     return (
-      <div className="blogCon"></div>
+      <div className="blogCon">
+        <div className="topBg"></div>
+        <div className="blogRight">
+          <div className="rCon">
+            <h3 className="title">关于Hexo主题制作</h3>
+            <p className="data">2018-02-22</p>
+            <div className="content">
+
+            </div>
+          </div>
+        </div>
+        <div className="blogLeft">
+          <div className="lCon"></div>
+        </div>
+      </div>
     )
   }
 
